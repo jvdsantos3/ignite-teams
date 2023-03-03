60efb5d5985ecb44ec825daf3302ebc024afccfb
João <jv.dossantos012@gmail.com>
2023-03-03 19:34:04 -0300

    Projet Ignite Teams

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,20 @@
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+
+import { Container } from './styles';
+import { GroupCard } from '@components/GroupCard';
+
+export function Groups() {
+  return (
+    <Container>
+      <Header />
+
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com a sua turma'
+      />
+
+      <GroupCard title='Galera do ignite' />
+    </Container>
+  );
+}
